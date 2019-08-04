968f8203e3ff06c27b49202794df445aef4a2148
Henry Chao <kaidouya0527@gmail.com>
2019-08-04 18:56:34 +0800

    Updates

---
+++ precache-manifest.7a483a05c4c7f223a1b0b42017c4dd2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b01e63e5b8b11ab46721c487a0ffc3",
+    "url": "/OoxxGame/index.html"
+  },
+  {
+    "revision": "2d8b19157e9578bc93ae",
+    "url": "/OoxxGame/static/js/2.1093115a.chunk.js"
+  },
+  {
+    "revision": "41ecbb57021af82f320b",
+    "url": "/OoxxGame/static/js/main.0e525d94.chunk.js"
+  },
+  {
+    "revision": "2ab3938c2f70d2436014",
+    "url": "/OoxxGame/static/js/runtime~main.036ec19e.js"
+  }
+]);
