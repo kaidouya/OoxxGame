006e083d2d48af9f310168df42f9688376b68469
pinsen_chao <henry.chao@skylinetw.com>
2020-09-16 16:57:53 +0800

    test

--- src/NewOoxx.js
+++ src/NewOoxx.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
-const newOoxx = () => {
-  return <div>I am new Ooxx</div>;
-};
+import './style/index.css';
 
-export default newOoxx;
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+class NewOoxx extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header className="header">
+          <div className="logo"></div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu
+                key="sun1"
+                icon={<UserOutlined />}
+                title="subnav 1"
+              ></SubMenu>
+            </Menu>
+          </Sider>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+export default NewOoxx;
